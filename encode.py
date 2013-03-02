2c3009fe3820d938bd9bbf434ab88fc6e71f2b33
kedzie <mark.kedzierski@gmail.com>
2013-03-02 17:18:33 -0600

    merged into single python file

--- encode.py
+++ encode.py
@@ -1,9 +1,29 @@
-#!/usr/bin/python
+"""2-Pass x264 encoding with ffmpeg"""
 
+from subprocess import call
+from os.path import splitext
 from argparse import ArgumentParser
 import sys
-import ffmpeg
 
+def encode(files, bit_rate="2M", profile="main", preset="fast", audio="-c:a copy -map 0", extra="", extension="mp4", dry=False):
+	"""
+	Perform 2-pass encoding of multiple files using ffmpeg in operating system
+	"""
+	for infile in files:
+		print 'Processing '+infile+'...'
+		basename = splitext(infile)[0]
+		outfile = basename+'.'+extension
+		core='ffmpeg -i ' + infile + ' -threads auto -c:v libx264 -tune film -preset ' + preset + ' -profile:v ' + profile + ' -b:v ' + bit_rate + ' -passlogfile ' + basename + ' ' + extra;
+		firstpass=core+' -pass 1 -an -f rawvideo -y /dev/null'
+		secondpass=core+' -pass 2 ' + audio + ' ' + outfile
+		if(dry):
+			print(firstpass)
+			print(secondpass)
+		else:
+			call(firstpass.split())
+			call(secondpass.split())
+			call(['rm', basename+'-0.log', basename+'-0.log.mbtree'])
+	
 if __name__ == "__main__":
 	parser = ArgumentParser(description='X264 Encoder')
 	parser.add_argument('files', nargs='+', help='files to encode (wildcard)')
